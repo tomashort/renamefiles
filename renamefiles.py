ac8f35ee2e6ce127cebb8b6567d883066799f419
Tom Short <tom.a.short@gmail.com>
2018-11-27 19:51:24 +0000

    Add file list functionality

---
+++ renamefiles.py
@@ -0,0 +1,8 @@
+import os
+def rename_files():
+    # Get file names from folder
+    file_list = os.listdir(r"C:\Users\Tom\Documents")
+    print(file_list)
+    # Rename each file
+
+rename_files()
