5a250a27ae4f204d632b20a9561f8b11a5bc4d3c
Tom Short <tom.a.short@gmail.com>
2018-11-27 20:41:09 +0000

    Add file renaming functionality

--- renamefiles.py
+++ renamefiles.py
@@ -1,8 +1,17 @@
 import os
-def rename_files():
-    # Get file names from folder
-    file_list = os.listdir(r"C:\Users\Tom\Documents")
-    print(file_list)
-    # Rename each file
 
-rename_files()
+def rename_files(dir):
+    # Set current working directory
+    os.chdir(dir)
+
+    # Get file names from folder
+    file_list = os.listdir(dir)
+    
+    # Rename each file
+    for file_name in file_list:
+        new_name = file_name.translate(file_name.maketrans('', '', '0123456789'))
+        if not new_name == file_name:
+            os.rename(file_name, new_name)
+            print(f"Renamed {file_name} to {new_name}")
+    
+rename_files(r"C:\Users\Tom\Documents\Code\Udacity\Python\secretmessage")
